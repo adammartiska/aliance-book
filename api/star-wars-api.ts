06394bce59c11eafaebd17a6a4c52389b4aa1a52
Adam Martiska <adam.martiska@kistler.com>
2025-03-05 13:13:51 +0100

    feat: Add method for fetching character detail

--- api/star-wars-api.ts
+++ api/star-wars-api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+import { StarWarsCharacterDto } from '../types/star-wars-character.dto';
 import { StarWarsCharactersResponseDto } from '../types/star-wars-characters-response.dto';
 import { SWAPI_BASE_URL } from '../utils/constants';
 
@@ -11,6 +12,16 @@ export const getCharacters = async (search?: string, page?: number) => {
         page,
       },
     });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching characters:', error);
+    throw error;
+  }
+};
+
+export const getCharacterById = async (id: number) => {
+  try {
+    const response = await axios.get<StarWarsCharacterDto>(`${SWAPI_BASE_URL}/people/${id}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching characters:', error);
